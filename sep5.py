e6722f552b972fc20109ba35989b45d6a351811e
ladazkalii <ladazkalii@gmail.com>
2024-12-24 21:36:50 +0700

    Update sep5.py

--- sep5.py
+++ sep5.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://aa1bbb42ebf8047790d5__cr.sg:005d3d6538c06a08@gw.dataimpulse.com:10005',
-        'https': 'https://aa1bbb42ebf8047790d5__cr.sg:005d3d6538c06a08@gw.dataimpulse.com:10005',
+        'http': 'http://8498ba791056eb98d8d3__cr.sg:42c5ae9a89b56bc9@gw.dataimpulse.com:10005',
+        'https': 'https://8498ba791056eb98d8d3__cr.sg:42c5ae9a89b56bc9@gw.dataimpulse.com:10005',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
