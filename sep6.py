1c66dcdd76648be8c39bf1fd0b7c1dd35c437195
ladazkalii <ladazkalii@gmail.com>
2024-12-25 22:06:28 +0700

    Update sep6.py

--- sep6.py
+++ sep6.py
@@ -26,7 +26,7 @@ print(driver.page_source)
 
 time.sleep(1)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/d255bc19-d483-4d58-8a97-f755e986e4b0")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/8aa9215e-0be4-4ee9-953f-414c21e7ce73")
 time.sleep(50)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
