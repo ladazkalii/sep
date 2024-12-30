afe8589e75fb4ca98f8e5f759ebb787287fdba2e
ladazkalii <ladazkalii@gmail.com>
2024-12-30 23:10:38 +0700

    Update sep6.py

--- sep6.py
+++ sep6.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://geonode_z45cq1QkSt:bb9069c5-ec91-4ae4-99b8-32b1fe09d3d3@premium-residential.geonode.com:10106',
-        'https': 'https://geonode_z45cq1QkSt:bb9069c5-ec91-4ae4-99b8-32b1fe09d3d3@premium-residential.geonode.com:10106',
+        'http': 'http://geonode_z45cq1QkSt:bb9069c5-ec91-4ae4-99b8-32b1fe09d3d3@premium-residential.geonode.com:9006',
+        'https': 'https://geonode_z45cq1QkSt:bb9069c5-ec91-4ae4-99b8-32b1fe09d3d3@premium-residential.geonode.com:9006',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -26,7 +26,7 @@ print(driver.page_source)
 
 time.sleep(1)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/9fd18f7e-4e14-4cf4-b6c0-9365d05da95f")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/4aeba2ee-bdde-4b9c-90af-6cd109be6482")
 time.sleep(50)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
