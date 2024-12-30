c724ae836b43bca112c54f14ca4720d58d8d7f79
ladazkalii <ladazkalii@gmail.com>
2024-12-31 03:43:19 +0700

    Update sep9.py

--- sep9.py
+++ sep9.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://geonode_z45cq1QkSt:bb9069c5-ec91-4ae4-99b8-32b1fe09d3d3@premium-residential.geonode.com:10009',
-        'https': 'https://geonode_z45cq1QkSt:bb9069c5-ec91-4ae4-99b8-32b1fe09d3d3@premium-residential.geonode.com:10009',
+        'http': 'http://geonode_z45cq1QkSt:bb9069c5-ec91-4ae4-99b8-32b1fe09d3d3@premium-residential.geonode.com:9009',
+        'https': 'https://geonode_z45cq1QkSt:bb9069c5-ec91-4ae4-99b8-32b1fe09d3d3@premium-residential.geonode.com:9009',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -20,11 +20,6 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
-
-driver.get('https://httpbin.org/ip')
-print(driver.page_source)
-
-time.sleep(1)
 
 driver.get("https://sepolia-faucet.pk910.de/#/mine/ce95d510-f913-4c1c-ae30-bea5792eacba")
 time.sleep(50)
