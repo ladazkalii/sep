e07ac385373ec4fbd9d433be7dd3bf65a6c11b4f
ladazkalii <ladazkalii@gmail.com>
2024-12-31 07:13:10 +0700

    Update sep5.py

--- sep5.py
+++ sep5.py
@@ -21,13 +21,8 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get('https://httpbin.org/ip')
-print(driver.page_source)
-
-time.sleep(1)
-
 driver.get("https://sepolia-faucet.pk910.de/#/mine/726e9a37-60a4-42b1-8781-d6a37da53e43")
-time.sleep(50)
+time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
 content_text = div_element.text
