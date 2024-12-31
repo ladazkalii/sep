3c6c2299ea1b43439d4614ce9a8fd997c63b262e
ladazkalii <ladazkalii@gmail.com>
2024-12-31 13:29:40 +0700

    Update sep3.py

--- sep3.py
+++ sep3.py
@@ -18,10 +18,20 @@ chrome_options.add_argument("--window-size=1920, 1200")
 # Hapus '--headless' untuk melihat apakah ekstensi berjalan dengan benar dalam mode normal
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--disable-background-networking")  # Mencegah koneksi jaringan di latar belakang
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument("--disable-client-side-phishing-detection")  # Nonaktifkan deteksi phishing
+chrome_options.add_argument("--disable-default-apps")  # Nonaktifkan aplikasi bawaan Chrome
+chrome_options.add_argument("--disable-features=NetworkPrediction")  # Nonaktifkan prediksi jaringan
+chrome_options.add_argument("--disable-sync")  # Nonaktifkan sinkronisasi
+chrome_options.add_argument("--metrics-recording-only")  # Nonaktifkan pengumpulan data
+chrome_options.add_argument("--safebrowsing-disable-auto-update")  # Nonaktifkan pembaruan otomatis Safe Browsing
+chrome_options.add_argument("--disable-component-update")  # Nonaktifkan pembaruan komponen
+chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keandalan domain
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/c667c961-161f-4d6d-ba46-f8aec1509bd9")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/b278d99a-1088-4496-9a37-bfac18ebba4d")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
