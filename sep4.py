5b562d1f406b4c4447a1348adf026a9debc4ec92
ladazkalii <ladazkalii@gmail.com>
2025-01-11 04:59:44 +0700

    Update sep4.py

--- sep4.py
+++ sep4.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://3fce0fb4071e0d7de068__cr.sl:13b3d30d22798ef3@gw.dataimpulse.com:10004',
-        'https': 'https://3fce0fb4071e0d7de068__cr.sl:13b3d30d22798ef3@gw.dataimpulse.com:10004',
+        'http': 'http://51e67c8aa404adfb8754__cr.sl:46ec33765c9a7249@gw.dataimpulse.com:10004',
+        'https': 'https://51e67c8aa404adfb8754__cr.sl:46ec33765c9a7249@gw.dataimpulse.com:10004',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
