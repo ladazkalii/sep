c3f3def61ff1da4fb29c2476201ac2bda1cf1288
ladazkalii <ladazkalii@gmail.com>
2025-01-16 18:40:41 +0700

    Update sep6.py

--- sep6.py
+++ sep6.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://a8a3ad31ca5d3c7b0364__cr.br:8c1ef8dbd9006341@gw.dataimpulse.com:10006',
-        'https': 'https://a8a3ad31ca5d3c7b0364__cr.br:8c1ef8dbd9006341@gw.dataimpulse.com:10006',
+        'http': 'http://4c25eb02b7aeec47fb7d__cr.br:15ae88c48217779b@gw.dataimpulse.com:10006',
+        'https': 'https://4c25eb02b7aeec47fb7d__cr.br:15ae88c48217779b@gw.dataimpulse.com:10006',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
