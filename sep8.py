7346ae43c2a303689508847ae5d170f0d239e08b
ladazkalii <ladazkalii@gmail.com>
2025-01-24 21:51:09 +0700

    Update sep8.py

--- sep8.py
+++ sep8.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://21f4b9ce980d2a0e24c7__cr.pe:88f41e3ac77db28a@gw.dataimpulse.com:10008',
-        'https': 'https://21f4b9ce980d2a0e24c7__cr.pe:88f41e3ac77db28a@gw.dataimpulse.com:10008',
+        'http': 'http://b03a53d34673b12d21db__cr.pe:5f233201d21c2864@gw.dataimpulse.com:10008',
+        'https': 'https://b03a53d34673b12d21db__cr.pe:5f233201d21c2864@gw.dataimpulse.com:10008',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
