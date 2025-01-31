17f1068b3b1cd6f7d86f6c38fe84c5f2ea366882
ladazkalii <ladazkalii@gmail.com>
2025-02-01 00:50:48 +0700

    Update sep10.py

--- sep10.py
+++ sep10.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://6de35104959bff96139c__cr.fr:f747a2d41c6f65b1@gw.dataimpulse.com:10110',
-        'https': 'https://6de35104959bff96139c__cr.fr:f747a2d41c6f65b1@gw.dataimpulse.com:10110',
+        'http': 'http://6de35104959bff96139c__cr.fr:f747a2d41c6f65b1@gw.dataimpulse.com:10111',
+        'https': 'https://6de35104959bff96139c__cr.fr:f747a2d41c6f65b1@gw.dataimpulse.com:10111',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
