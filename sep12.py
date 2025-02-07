aa3e5321a8a588faaf9dcea60d2d625f4146859e
ladazkalii <ladazkalii@gmail.com>
2025-02-07 18:56:43 +0700

    Update sep12.py

--- sep12.py
+++ sep12.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://2f6fcce455211a373d2c__cr.uk:d9e0379371262820@gw.dataimpulse.com:10112',
-        'https': 'https://2f6fcce455211a373d2c__cr.uk:d9e0379371262820@gw.dataimpulse.com:10112',
+        'http': 'http://69cc1ad8006c2f31f665__cr.sl:31c41583310000e2@gw.dataimpulse.com:10112',
+        'https': 'https://69cc1ad8006c2f31f665__cr.sl:31c41583310000e2@gw.dataimpulse.com:10112',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
