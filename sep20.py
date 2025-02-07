2a29b42f049e5b5cd75d2c4f822381d2f2fd9f3d
ladazkalii <ladazkalii@gmail.com>
2025-02-07 22:52:20 +0700

    Update sep20.py

--- sep20.py
+++ sep20.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://2f6fcce455211a373d2c__cr.au:d9e0379371262820@gw.dataimpulse.com:10120',
-        'https': 'https://2f6fcce455211a373d2c__cr.au:d9e0379371262820@gw.dataimpulse.com:10120',
+        'http': 'http://69cc1ad8006c2f31f665__cr.au:31c41583310000e2@gw.dataimpulse.com:10120',
+        'https': 'https://69cc1ad8006c2f31f665__cr.au:31c41583310000e2@gw.dataimpulse.com:10120',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
