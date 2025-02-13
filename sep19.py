74f6ee3f823ed59b4c1f261ea33deb9e7c3c4648
ladazkalii <ladazkalii@gmail.com>
2025-02-13 18:27:47 +0700

    Update sep19.py

--- sep19.py
+++ sep19.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://7ba582e59e4babcfe6fe__cr.co:4e708803dc4a41f1@gw.dataimpulse.com:10019',
-        'https': 'https://7ba582e59e4babcfe6fe__cr.co:4e708803dc4a41f1@gw.dataimpulse.com:10019',
+        'http': 'http://38ccb09ae81123f8e47b__cr.co:071a482278d0d146@gw.dataimpulse.com:10019',
+        'https': 'https://38ccb09ae81123f8e47b__cr.co:071a482278d0d146@gw.dataimpulse.com:10019',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
