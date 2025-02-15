94bd8691a76ca3ba3584c8d4036488ad53bdd06d
ladazkalii <ladazkalii@gmail.com>
2025-02-15 19:56:29 +0700

    Update sep17.py

--- sep17.py
+++ sep17.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://25372128-zone-custom-region-PE-sessid-N0cb42lK-sessTime-120:gvgsm5NI@aus.360s5.com:3600',
-        'https': 'https://25372128-zone-custom-region-PE-sessid-N0cb42lK-sessTime-120:gvgsm5NI@aus.360s5.com:3600',
+        'http': 'http://e6a745096e2a5300e1bc__cr.it:bb3cc85ff8577baa@gw.dataimpulse.com:10017',
+        'https': 'https://e6a745096e2a5300e1bc__cr.it:bb3cc85ff8577baa@gw.dataimpulse.com:10017',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
