e463a43fd131344bbb3c73e69360d7b320e7f8ef
ladazkalii <ladazkalii@gmail.com>
2025-02-15 16:19:51 +0700

    Update sep4.py

--- sep4.py
+++ sep4.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://25372128-zone-custom-region-BD-sessid-lpgwZKID-sessTime-120:gvgsm5NI@asg.360s5.com:3600',
-        'https': 'https://25372128-zone-custom-region-BD-sessid-lpgwZKID-sessTime-120:gvgsm5NI@asg.360s5.com:3600',
+        'http': 'http://25372128-zone-custom-region-HK-sessid-Wo04Rl5I-sessTime-105:gvgsm5NI@asg.360s5.com:3600',
+        'https': 'https://25372128-zone-custom-region-HK-sessid-Wo04Rl5I-sessTime-105:gvgsm5NI@asg.360s5.com:3600',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
