2041e7debfd960cb33b4acdaf42337589e74d2b0
ladazkalii <ladazkalii@gmail.com>
2025-05-06 12:58:54 +0700

    Update sep1.py

--- sep1.py
+++ sep1.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://d8e500d03fdb94e45fc4__cr.jm:5d58840656e914d2@gw.dataimpulse.com:10001',
-        'https': 'https://d8e500d03fdb94e45fc4__cr.jm:5d58840656e914d2@gw.dataimpulse.com:10001',
+        'http': 'http://8c5906b99fbd1c0bcd0f916d545c565adef1b53038ada30b41845627f276a26ae1e851cc35fe867fce315eeb38f128feab7de56bf749bc8adcd2de8f2c36a875:hy6gsyfv9gff@proxy.toolip.io:31111',
+        'https': 'https://8c5906b99fbd1c0bcd0f916d545c565adef1b53038ada30b41845627f276a26ae1e851cc35fe867fce315eeb38f128feab7de56bf749bc8adcd2de8f2c36a875:hy6gsyfv9gff@proxy.toolip.io:31111',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -18,7 +18,7 @@ chrome_options.add_argument('--headless')
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/c7a927a2-c43d-458a-8339-28986ced9e23")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/4d053b32-581f-4cc5-a34d-142c4c77e621")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
