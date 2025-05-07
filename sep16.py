d0f392ece32d0f66b66cc26ce358085744147da0
ladazkalii <ladazkalii@gmail.com>
2025-05-07 22:00:56 +0700

    Update sep16.py

--- sep16.py
+++ sep16.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://e697011ce4c77b244d4d__cr.tn:10c89834dc62100a@gw.dataimpulse.com:10116',
-        'https': 'https://e697011ce4c77b244d4d__cr.tn:10c89834dc62100a@gw.dataimpulse.com:10116',
+        'http': 'http://53024118a3986974ae6e__cr.tn:c1906fb5efefe51b@gw.dataimpulse.com:10116',
+        'https': 'https://53024118a3986974ae6e__cr.tn:c1906fb5efefe51b@gw.dataimpulse.com:10116',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -31,7 +31,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/90009111-4863-404c-9a8c-92211f568a6a")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/a133ae46-6285-410f-a3dc-60c1f841c00d")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
