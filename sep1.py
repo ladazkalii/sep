6dde2b35610d1ef21366dee33e98c1129b1aacd9
ladazkalii <ladazkalii@gmail.com>
2025-05-08 13:15:15 +0700

    Update sep1.py

--- sep1.py
+++ sep1.py
@@ -18,7 +18,7 @@ chrome_options.add_argument('--headless')
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/06db1fbd-b325-46b1-a41b-57331ef0bb7c")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/ac8e9f6f-c0b8-445f-8432-87169a80a74d")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
