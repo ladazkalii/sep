9b402072bc5807aa6d6c2068101b3c717b8aaac9
ladazkalii <ladazkalii@gmail.com>
2025-05-08 18:55:55 +0700

    Update sep3.py

--- sep3.py
+++ sep3.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://53024118a3986974ae6e__cr.ve:c1906fb5efefe51b@gw.dataimpulse.com:10103',
-        'https': 'https://53024118a3986974ae6e__cr.ve:c1906fb5efefe51b@gw.dataimpulse.com:10103',
+        'http': 'http://2a9eb19d29cea23a99ba__cr.ve:bc0af6a2f278b9d2@gw.dataimpulse.com:10103',
+        'https': 'https://2a9eb19d29cea23a99ba__cr.ve:bc0af6a2f278b9d2@gw.dataimpulse.com:10103',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
